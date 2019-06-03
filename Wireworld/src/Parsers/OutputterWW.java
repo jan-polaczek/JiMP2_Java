1a6b1fe6d584441e472d33de27a34b43f980d8e0
jan-polaczek <jan.polaczek@interia.pl>
2019-06-03 18:34:22 +0200

    created files

---
+++ Wireworld/src/Parsers/OutputterWW.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Parsers;
+
+/**
+ *
+ * @author 01133123
+ */
+public class OutputterWW implements Outputter {
+    
+}
