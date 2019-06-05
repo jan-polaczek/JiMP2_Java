0a40db403bd3d51f0e6d00a4a8457f8d1ba7d582
jan-polaczek <jan.polaczek@interia.pl>
2019-06-05 19:12:05 +0200

    floating rectangle

---
+++ Wireworld/src/Views/Main.java
@@ -0,0 +1,46 @@
+
+package Views;
+
+import java.util.Timer;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+import java.util.TimerTask;
+import javafx.scene.shape.Rectangle;
+import javafx.scene.Group;
+public class Main extends Application {
+    private static final int REFRESH_TIME = 400;
+    
+    @Override
+    public void start(Stage stage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("FXMLDocument.fxml"));
+        stage.initStyle(StageStyle.UNDECORATED);
+        Group rootGroup = new Group();
+        rootGroup.getChildren().add( root );
+        float x = 100, y = 100;
+        Rectangle rectangle = new Rectangle(x, y, 50, 50);
+        
+        Timer timer = new Timer();
+        timer.scheduleAtFixedRate(new TimerTask() {
+            @Override
+            public void run() {
+                rectangle.setX(rectangle.getX()+50f);
+            }
+        }, 0, REFRESH_TIME);
+        
+        rootGroup.getChildren().add(rectangle);
+        
+        Scene scene = new Scene(rootGroup);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        
+        launch(args);
+    }
+    
+}
