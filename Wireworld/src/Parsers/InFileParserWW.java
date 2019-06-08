34e032e85cf107ba0df996d9c87461419b3f997d
mozdzyk1 <promaestor@gmail.com>
2019-06-08 18:54:08 +0200

    Structures added to InFileParserWW

--- Wireworld/src/Parsers/InFileParserWW.java
+++ Wireworld/src/Parsers/InFileParserWW.java
@@ -37,21 +37,180 @@ public class InFileParserWW implements InFileParser {
         {
             String line[] = sc.nextLine().split(" ");
             String type = line[0];
-            if(type == "Diode")
+            if("Diode".equals(type))
             {
+                int x = Integer.valueOf(line[1]);
+                int y = Integer.valueOf(line[2]);
+                String z = String.valueOf(line[3]);
+                if("Normal".equals(z))
+                {
+                grid.getCell(x, y).setColor('y');
+                grid.getCell(x+1, y).setColor('y');
+                grid.getCell(x, y-1).setColor('y');
+                grid.getCell(x, y-2).setColor('y');
+                grid.getCell(x+1, y-2).setColor('y');
+                }
+                else if("Reversed".equals(z))
+                {
+                grid.getCell(x, y).setColor('y');
+                grid.getCell(x+1, y).setColor('y');
+                grid.getCell(x+1, y-1).setColor('y');
+                grid.getCell(x, y-2).setColor('y');
+                grid.getCell(x+1, y-2).setColor('y'); 
+                }
+            }
+            else if("Conductor".equals(type))
+            {
+                int x1 = Integer.valueOf(line[1]);
+                int y1 = Integer.valueOf(line[2]);
+                int x2 = Integer.valueOf(line[3]);
+                int y2 = Integer.valueOf(line[4]);
+                if(x1 <= x2)
+                {
+                    for(int i=x1;i<=x2;i++)
+                    {
+                        grid.getCell(i, y1).setColor('y');
+                    }
+                }
+                else
+                {
+                    for(int i=x2;i<=x1;i++)
+                    {
+                        grid.getCell(i, y1).setColor('y');
+                    }  
+                }
+                if(y1 <= y2)
+                {
+                    for(int i=y1;i<=y2;i++)
+                    {
+                        grid.getCell(x2, i).setColor('y');
+                    }
+                }
+                else
+                {
+                    for(int i=y1;i>=y2;i--)
+                    {
+                        grid.getCell(i, y1).setColor('y');
+                    }  
+                }
                 
             }
-            else if(type == "AndGate")
+            else if("AndGate".equals(type))
             {
-                
+                int x = Integer.valueOf(line[1]);
+                int y = Integer.valueOf(line[2]);
+                String z = String.valueOf(line[3]);
+                if("Normal".equals(z))
+                    {
+                    grid.getCell(x, y).setColor('y');
+                    grid.getCell(x, y-1).setColor('y');
+                    grid.getCell(x, y-2).setColor('y');
+                    grid.getCell(x, y-4).setColor('y');
+                    grid.getCell(x, y-5).setColor('y');
+                    grid.getCell(x, y-6).setColor('y');
+                    grid.getCell(x+1, y).setColor('y');
+                    grid.getCell(x+1, y-2).setColor('y');
+                    grid.getCell(x+1, y-4).setColor('y');
+                    grid.getCell(x+1, y-6).setColor('y');
+                    grid.getCell(x+2, y-1).setColor('y');
+                    grid.getCell(x+2, y-5).setColor('y');
+                    grid.getCell(x+3, y-1).setColor('y');
+                    grid.getCell(x+3, y-2).setColor('y');
+                    grid.getCell(x+3, y-3).setColor('y');
+                    grid.getCell(x+3, y-4).setColor('y');
+                    grid.getCell(x+3, y-5).setColor('y');
+                    grid.getCell(x+4, y-2).setColor('y');
+                    grid.getCell(x+4, y-4).setColor('y');
+                    grid.getCell(x+5, y-3).setColor('y');
+                    }
+                else if("Reversed".equals(z))
+                {
+                    grid.getCell(x, y-3).setColor('y');
+                    grid.getCell(x+1, y-2).setColor('y');
+                    grid.getCell(x+1, y-4).setColor('y');
+                    grid.getCell(x+2, y-1).setColor('y');
+                    grid.getCell(x+2, y-2).setColor('y');
+                    grid.getCell(x+2, y-3).setColor('y');
+                    grid.getCell(x+2, y-4).setColor('y');
+                    grid.getCell(x+2, y-5).setColor('y');
+                    grid.getCell(x+3, y-1).setColor('y');
+                    grid.getCell(x+3, y-5).setColor('y');
+                    grid.getCell(x+4, y).setColor('y');
+                    grid.getCell(x+4, y-2).setColor('y');
+                    grid.getCell(x+4, y-4).setColor('y');
+                    grid.getCell(x+4, y-6).setColor('y');
+                    grid.getCell(x+5, y).setColor('y');
+                    grid.getCell(x+5, y-1).setColor('y');
+                    grid.getCell(x+5, y-2).setColor('y');
+                    grid.getCell(x+5, y-4).setColor('y');
+                    grid.getCell(x+5, y-5).setColor('y');
+                    grid.getCell(x+5, y-6).setColor('y');
+                    
+                }
             }
-            else if(type == "OrGate")
+            else if("OrGate".equals(type))
             {
-                
+                int x = Integer.valueOf(line[1]);
+                int y = Integer.valueOf(line[2]);
+                String z = String.valueOf(line[3]);
+                if("Normal".equals(z))
+                {
+                    grid.getCell(x, y).setColor('y');
+                    grid.getCell(x, y-2).setColor('y');
+                    grid.getCell(x, y-4).setColor('y');
+                    grid.getCell(x+1, y-1).setColor('y');
+                    grid.getCell(x+1, y-2).setColor('y');
+                    grid.getCell(x+1, y-3).setColor('y');
+                }
+                else if("Reversed".equals(z))
+                {
+                    grid.getCell(x, y-1).setColor('y');
+                    grid.getCell(x, y-2).setColor('y');
+                    grid.getCell(x, y-3).setColor('y');
+                    grid.getCell(x+1, y).setColor('y');
+                    grid.getCell(x+1, y-2).setColor('y');
+                    grid.getCell(x+1, y-4).setColor('y');
+                }
             }
-            else if(type == "XorGate")
+            else if("XorGate".equals(type))
             {
-                
+                int x = Integer.valueOf(line[1]);
+                int y = Integer.valueOf(line[2]);
+                String z = String.valueOf(line[3]);
+                if("Normal".equals(z))
+                {
+                    grid.getCell(x, y).setColor('y');
+                    grid.getCell(x, y-2).setColor('y');
+                    grid.getCell(x, y-3).setColor('y');
+                    grid.getCell(x, y-4).setColor('y');
+                    grid.getCell(x, y-6).setColor('y');
+                    grid.getCell(x+1, y-1).setColor('y');
+                    grid.getCell(x+1, y-2).setColor('y');
+                    grid.getCell(x+1, y-4).setColor('y');
+                    grid.getCell(x+1, y-5).setColor('y');
+                    grid.getCell(x+2, y-2).setColor('y');
+                    grid.getCell(x+2, y-4).setColor('y');
+                    grid.getCell(x+3, y-2).setColor('y');
+                    grid.getCell(x+3, y-3).setColor('y');
+                    grid.getCell(x+3, y-4).setColor('y');
+                }
+                else if("Reversed".equals(z))
+                {
+                    grid.getCell(x, y-2).setColor('y');
+                    grid.getCell(x, y-3).setColor('y');
+                    grid.getCell(x, y-4).setColor('y');
+                    grid.getCell(x+1, y-2).setColor('y');
+                    grid.getCell(x+1, y-4).setColor('y');
+                    grid.getCell(x+2, y-1).setColor('y');
+                    grid.getCell(x+2, y-2).setColor('y');
+                    grid.getCell(x+2, y-4).setColor('y');
+                    grid.getCell(x+2, y-5).setColor('y');
+                    grid.getCell(x+3, y).setColor('y');
+                    grid.getCell(x+3, y-2).setColor('y');
+                    grid.getCell(x+3, y-3).setColor('y');
+                    grid.getCell(x+3, y-4).setColor('y');
+                    grid.getCell(x+3, y-6).setColor('y'); 
+                }
             }
             else if("ElectronHead".equals(type))
             {
